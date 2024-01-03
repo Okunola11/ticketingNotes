aa3b41c3b74af84bc85084dfa673894e06475a04
Abdulwasiu <abdayoade@gmail.com>
2024-01-03 21:09:45 +0100

    Added logOut and refresh mutations to the apiSlice

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -1,4 +1,5 @@
 import { apiSlice } from "../../app/api/apiSlice";
+import { setCredentials, logOut } from "./authSlice";
 
 export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -9,7 +10,42 @@ export const authApiSlice = apiSlice.injectEndpoints({
         body: { ...credentials },
       }),
     }),
+    refresh: builder.mutation({
+      query: () => ({
+        url: "/auth/pops",
+        method: "GET",
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          console.log(data);
+          const { accessToken } = data;
+          dispatch(setCredentials(accessToken));
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    }),
+    sendLogout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          console.log(data);
+          dispatch(logOut());
+          setTimeout(() => {
+            dispatch(apiSlice.util.resetApiState());
+          }, 1000);
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    }),
   }),
 });
 
-export const { useLoginMutation } = authApiSlice;
+export const { useLoginMutation, useRefreshMutation, useSendLogoutMutation } =
+  authApiSlice;
