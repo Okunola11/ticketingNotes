f170897b0f66039093890dcc556152da20f290af
Abdulwasiu <abdayoade@gmail.com>
2024-01-03 06:53:05 +0100

    Added authSlice and updated the apiSlice to allow reauth/refresh

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { user: null, token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logOut: (state, action) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logOut } = authSlice.actions;
+
+export default authSlice.reducer;
